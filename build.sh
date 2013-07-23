b412b2c4c1dec17abb7a169a3ec6022d6d4dc244
vellvisher <vaarnan@gmail.com>
2013-07-23 00:22:00 -0400

    Huge revamp, added Google

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ pdflatex Resume.tex
 latex Resume.tex
 #|sed -re "s/I build.+\./I build stuff\./g"
 catdvi -e 0 -U Resume.dvi | sed -re "s/([^^[:space:]])\s+/\1 /g"\
-|sed -e 's/^[ \t]*//' | sed -re "s/(\.( \.)+)$//g" | sed '1d'|sed -re "s/○/•/g" \
+|sed -e 's/^[ \t]*//' | sed -re "s/(\.( \.)+)$//g" | sed -re "s/○/•/g" \
 |sed -e 's/�/☎/g' | sed -re "s///g" |sed -re "s/ﬀ/ff/g" | sed -re "s/ﬂ/fl/g" |iconv > Resume.txt
