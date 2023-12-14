bf3636a9fdcb873672a3e9f5e4ac14d60f382746
Vaarnan Drolia <vaarnan@gmail.com>
2023-12-14 11:45:46 +0000

    remove old build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Form feed
-pdflatex Resume.tex
-latex Resume.tex
-#|sed -re "s/I build.+\./I build stuff\./g"
-catdvi -e 0 -U Resume.dvi | sed -re "s/([^^[:space:]])\s+/\1 /g"\
-|sed -e 's/^[ \t]*//' | sed -re "s/(\.( \.)+)$//g" | sed -re "s/○/•/g" \
-|sed -e 's/�/☎/g' | sed -re "s///g" |sed -re "s/ﬀ/ff/g" | sed -re "s/ﬂ/fl/g" |iconv > Resume.txt
-cp Resume.txt Resume_VaarnanDrolia.txt
